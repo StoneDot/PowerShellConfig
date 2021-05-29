ba39c47624362670271abd99c88de6e05e380b3e
Hiroaki Goto <goto.inct@gmail.com>
2021-05-29 21:59:10 +0900

    Add github function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,7 +8,15 @@ Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
 function l { Get-ChildItem | Format-Wide Name -AutoSize }
 function cdr() { fd -H -t d -E .git -E node_modules | fzf | Set-Location }
 function cdz() { z -l | oss | Select-Object -skip 3 | ForEach-Object { $_ -split " +" } | Select-String -raw '^[a-zA-Z].+' | fzf | Set-Location }
-
+function dcr() { docker-compose run $args }
+function github() {
+    $github_remotes = git remote -v |
+    Where-Object { $_ -Like '*(fetch)*' } |
+    Select-Object { $_.Split()[1] } |
+    Where-Object { $_ -Like '*github.com/*' } |
+    Select-Object -First 1
+    Start-Process ($github_remotes[0].'$_.Split()[1]'.Replace(".git", ""))
+}
 
 # PowerShell Core7でもConsoleのデフォルトエンコーディングはsjisなので必要
 [System.Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
